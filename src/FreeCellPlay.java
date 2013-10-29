4d7804a571343b88f0768caa60ac28e131fc90c7
scottk-macbook <dscottkesler@gmail.com>
2013-10-28 21:15:19 -0500

    2013-10-28 9:15 PM

--- src/FreeCellPlay.java
+++ src/FreeCellPlay.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 
 public class FreeCellPlay {
 	private  Deck deck;
-	
+	//private  static String FILENAME = "moves.txt";
 	public FreeCellPlay() {
 
 	}
@@ -16,6 +17,42 @@ public class FreeCellPlay {
 			}
 		}
 		return test;
+	}
+	
+	public static boolean playGameByFile(GameBoard board, ArrayList<Integer> moves){
+		boolean play = true;
+		//Scanner line = new Scanner(System.in);
+		int source, destination;
+		//Scanner parse = new Scanner(input);
+			try{
+				do { 
+				System.out.println(board.toString());
+//				 System.out.println("Enter Source and Destination column, or '0' to QUIT ");
+//				 str = line.nextLine();
+//				 String [] moves= str.split(" ");
+	             source = moves.remove(0);
+	            if (source == 0) { 
+	              System.out.println("Q U I T  G A M E" );
+	          	  break;
+	          	}
+	            destination = moves.remove(0);
+	            
+	            System.out.println("Source is:" + source + "\nDesintation is:" + destination);
+	             if(! board.isMoveLegal(source, destination)){
+	            	 System.out.println("\n** ILLEGAL MOVE. Please try again **\n");
+	            	 return play;}
+	             if(isGameOver(board)){
+	            	 System.out.println("\n *** Congratulations. You win the game! ***");
+	             }
+	             board = new GameBoard(source, destination, board);
+			} while (source != 0);
+				play = false;
+				return play;
+			} catch(ArrayIndexOutOfBoundsException e) {
+				      //inform user that they have not entered two integers for source and destination
+				      System.out.println("\nOOPS! need source and destination for move. Try again!\n");
+				      return play;
+			} 
 	}
 	
 	public static boolean playGame(GameBoard board){
@@ -53,22 +90,31 @@ public class FreeCellPlay {
 				      System.out.println("\nOOPS! need source and destination for move. Try again!\n");
 				      return play;
 			} 
-	         
 	}
 
 	public static void main(String[] args) {
-		if(args.length > 0){
+		//if(args.length > 0){
+			String filename = "moves.txt";
+	        //String filename = args[0].toString();
+	        
 			try {
-		        String filename = args[0].toString();
-		        Scanner filescanner = new Scanner (new File(filename));
+				Scanner filescanner = new Scanner (new File(filename));
+		        ArrayList<Integer> moves = new ArrayList<Integer>();
+		        while(filescanner.hasNext()){
+		        	moves.add(filescanner.nextInt());
+		        }
+		        GameBoard board = new GameBoard();
+		        playGameByFile(board, moves);
+		        filescanner.close();
+		    
 		    } catch (FileNotFoundException e) {
-		        System.err.println("Argument" + "is not a filename");
-		        System.exit(1);
-		    }
+		        System.err.println("Argument " + filename + " is not a filename");
+		        System.exit(0);
 		}
-		GameBoard board = new GameBoard();
-		while(playGame(board)){
-			playGame(board);
-		} System.exit(0);
+//		GameBoard board = new GameBoard();
+//		while(playGame(board)){
+//			playGame(board);
+//			} System.exit(0);
+//		}
 	}
 }
