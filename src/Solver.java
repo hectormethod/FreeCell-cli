1d0e4501e19784d076d16d38785b1f9c0830c1ad
scottk_imac <dscottkesler@gmail.com>
2013-11-19 12:07:56 -0600

    created Solver() class

---
+++ src/Solver.java
@@ -0,0 +1,16 @@
+import java.util.TreeMap;
+
+
+public class Solver extends TreeMap<GameBoard>  {
+	
+	public Sover(){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
