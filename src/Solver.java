a1c59cbda9ac1cdc04a818e3980ca1305176c075
scottk-macbook <dscottkesler@gmail.com>
2013-12-03 16:38:15 -0600

    macbook - early chages?

--- src/Solver.java
+++ src/Solver.java
@@ -1,16 +1,49 @@
-import java.util.TreeMap;
+import java.util.ArrayList;
 
 
-public class Solver extends TreeMap<GameBoard>  {
+
+public class Solver {
+	private ArrayList<GameBoard> path = new ArrayList<GameBoard>();
 	
-	public Sover(){
+	public Solver(){
 		
 	}
 	
+	//** implement the brute force first!
+	public boolean isBruteSolution(GameBoard b){
+		return true;
+	}
+	
+	
+	public boolean isSolution(GameBoard b){ //recursive solution
+		//GameBoard board = new GameBoard(source, destination, b);
+		path.add(b);
+		boolean result = false;
+		int i = 0;
+		if (b.isBoardWon(b)){ //base case true, found winning board.
+			result=true;
+		}
+		
+		while (i < 16) {
+			b = new GameBoard(i, i+1, b);
+			if (isSolution(b)){
+				System.out.println("Valid path found");
+				result=true;
+			}
+			System.out.println("Invalid path removed");
+			path.remove(b);
+			return result;
+		}
+
+		return result;
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Deck deck = new Deck();
+		GameBoard b = new GameBoard(deck);
+		Solver solution = new Solver();
+		System.out.println(b);
+		System.out.println(solution.isSolution(b));
 	}
 
 }
